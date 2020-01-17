c0c0723d4ec342dfb1b7b09701f044656c388586
c.v.v.aswini.manasa <c.v.v.aswini.manasa@accenture.com>
2020-01-17 20:50:11 +0530

    Added unit test cases dependency libraries

--- app/src/test/java/com/example/codingexercise/ExampleUnitTest.kt
+++ app/src/test/java/com/example/codingexercise/ExampleUnitTest.kt
@@ -1,17 +1,36 @@
 package com.example.codingexercise
 
-import org.junit.Test
 
-import org.junit.Assert.*
+import android.os.AsyncTask
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.codingexercise.api.ApiInterface
+import com.example.codingexercise.api.DataRowsList
+import com.example.codingexercise.api.JsonResponse
+import com.example.codingexercise.repository.DataRepository
+import com.example.codingexercise.utils.Resource
+import com.example.codingexercise.utils.Status
+import com.example.codingexercise.viewmodel.DataViewModel
+import io.reactivex.Single
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+@RunWith(JUnit4::class)
+public class ExampleUnitTest {
+    @Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
 }
+
